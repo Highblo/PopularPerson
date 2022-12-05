69edd85f10d34984faee89fa1b5b36b52938ea67
siotan <nyaa.-525@outlook.jp>
2022-12-05 23:36:55 +0900

    first

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref } from "vue";
+
+const personsList = ref([]);
+const fashionablePerson = ref("");
+const addPerson = () => {
+  if (fashionablePerson.value === "") return;
+  personsList.value.push(fashionablePerson.value);
+  fashionablePerson.value = "";
+};
+</script>
+
+<template>
+  <div class="top">
+    <h1>流行人</h1>
+    <p>現在誰が流行っているか一目で分かるアプリです。</p>
+    <p>自分の中で話題になっている人を投稿してみよう！</p>
+    <br />
+    <form @submit.prevent>
+      <input type="text" v-model="fashionablePerson" />
+      <v-btn color="secondary" @click="addPerson">投稿</v-btn>
+    </form>
+  </div>
+  <div class="container">
+    <ul>
+      <li v-for="person in personsList">
+        <p>{{ person }}</p>
+        <span></span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.top {
+  height: 30vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+input {
+  padding: 4px;
+  border-radius: 3px;
+  margin-right: 12px;
+  background-color: white;
+  outline: none;
+}
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+li {
+  background-color: aquamarine;
+  padding: 8px;
+  border-radius: 4px;
+  color: black;
+  list-style: none;
+  margin-bottom: 24px;
+}
+</style>
